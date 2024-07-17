b774bcb0bdafca38e1ca1b19cf08b222a47d9a2b
PUP-LIBRARYPC3 <PUP-LIBRARYPC3@DESKTOP-N5OCNK2>
2024-07-17 16:10:40 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using FoodDelivery.Models;
+using FoodDelivery.Services;
+using System;
+
+namespace FoodDeliveryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodServices foodServices = new FoodServices();
+
+            // Display all available foods
+            var foods = foodServices.GetAvailableFoods();
+
+            foreach (var food in foods)
+            {
+                Console.WriteLine($"Name: {food.Name}, Price: {food.Price}, Available: {food.IsAvailable}");
+            }
+
+            // Other operations (add/update/delete) can be similarly invoked
+
+            Console.ReadLine();
+        }
+    }
+}
+
